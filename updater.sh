8d8bfb2637311f9f94169eeb515b6089b5e5840d
Realtox <39673627+Realtox@users.noreply.github.com>
2020-05-27 20:49:56 +0200

    Create Updater.sh

---
+++ updater.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "#########################"
+echo "## Systeminformations ##"
+echo ""
+echo "   Date: " `date`
+echo ""
+echo "#########################"
+
+echo "Update the System?"
+echo "Y/n"
+read ANS
+if [ "$ANS" == "Y" ]
+    then
+        echo "Starting"
+        sudo apt update
+        sudo apt upgrade
+        echo "Finish"
+    else
+        echo "Exit"
+        exit
+fi
